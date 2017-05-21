6ac2f377704a5f3056412162444a1f327717fa97
vijendiran <vijendiran95@gmail.com>
2017-05-21 19:07:24 +0530

    Portfolio is completed

---
+++ portfolio.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $(".whatsup").click(whatsupClicked);
+function whatsupClicked()
+ {
+  $(".myNumber").fadeToggle();
+}
+if ($('#back-to-top').length) {
+    var scrollTrigger = 100,
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
+})
